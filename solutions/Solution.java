f9d962b9c779870745f92750e0d6d22574af1c51
charan <charanramagiri2004@gmail.com>
2025-04-02 10:55:37 +0530

    Update Solution.java

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -1,6 +1,19 @@
 package solutions;
 public class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        
+        int n = nums.length;
+        int[] result = new int[n];
+
+        // Brute force comparison
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (nums[j] < nums[i]) {
+                    count++;
+                }
+            }
+            result[i] = count;
+        }
+        return result;
     }
 }
