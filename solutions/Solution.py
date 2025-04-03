08a05b6836b4224354eef8ab2b9b36bf72c17fe2
charan <charanramagiri2004@gmail.com>
2025-04-03 23:15:30 +0530

    Update Solution.py

--- solutions/Solution.py
+++ solutions/Solution.py
@@ -7,7 +7,7 @@ class Solution:
         
         for i in range(len(nums)):  # Iterate through each element
             count = 0
-            for j in range(len(nums)):  # Compare with every other elementjhjvuhibib
+            for j in range(len(nums)):  # Compare with every other element
                 if nums[j] < nums[i]:
                     count += 1
             result.append(count)
