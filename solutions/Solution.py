1cd23915575f001b4731a0d1cc45f88b356751c8
charan <charanramagiri2004@gmail.com>
2025-04-03 23:54:24 +0530

    Update Solution.py

--- solutions/Solution.py
+++ solutions/Solution.py
@@ -2,4 +2,4 @@ from typing import List
 __package__ = "solutions"  # Ensure the package is set correctly
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        
+        # write your code here
