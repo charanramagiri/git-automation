bd69958ac9144ab3c4d4d629afc75f56446b2181
charan <charanramagiri2004@gmail.com>
2025-04-03 14:45:53 +0530

    Update Solution.cpp

--- solutions/Solution.cpp
+++ solutions/Solution.cpp
@@ -4,6 +4,20 @@ using namespace std;
 class Solution {
     public:
         vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-            return ;
+            int n = nums.size();
+            vector<int> result(n, 0);
+
+            for (int i = 0; i < n; i++) {
+                int count = 0;
+                for (int j = 0; j < n; j++) {
+                    if (nums[j] < nums[i]) {
+                        count++;
+                    }
+                }
+                result[i] = count;
+            }
+            
+            return result;
         }
     };
+
