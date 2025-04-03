b7757c251be7bb6dfc49d16b7cbe2e0c976b14e1
charan <charanramagiri2004@gmail.com>
2025-04-03 21:18:14 +0530

    Update Solution.py

--- solutions/Solution.py
+++ solutions/Solution.py
@@ -3,13 +3,4 @@ from typing import List
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
         #write your code here
-        result = []
         
-        for i in range(len(nums)):  # Iterate through each element
-            count = 0
-            for j in range(len(nums)):  # Compare with every other elementjhjvuhibib
-                if nums[j] < nums[i]:
-                    count += 1
-            result.append(count)
-        
-        return result
