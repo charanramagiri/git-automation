dad751192688b2f25fc5bb8c6fe2695d03c4b529
charan <charanramagiri2004@gmail.com>
2025-04-08 14:15:12 +0530

    Update Solution.py

--- solutions/Solution.py
+++ solutions/Solution.py
@@ -3,12 +3,5 @@ __package__ = "solutions"  # Ensure the package is set correctly
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
         # write your code here
-        result = []
-        for i in range(len(nums)):
-            count = 0
-            for j in range(len(nums)):
-                if nums[j] < nums[i]:
-                    count += 1
-            result.append(count)
-        return result
+        
         
