b1c66c7f6f9390e09242944b90c7c6977b661e0c
charan <charanramagiri2004@gmail.com>
2025-04-08 14:16:16 +0530

    Update Solution.java

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -2,19 +2,5 @@ package solutions;
 public class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
         //write your code here
-        int n = nums.length;
-        int[] result = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = 0; j < n; j++) {
-                if (nums[j] < nums[i]) {
-                    count++;
-                }
-            }
-            result[i] = count;
-        }
-
-        return result;
     }
 }
